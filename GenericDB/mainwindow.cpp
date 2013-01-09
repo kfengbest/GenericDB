b90f9eed289110974f60688e1ddebc94b9505146
kfengbest <kfengbest@hotmail.com>
2013-01-09 22:07:25 +0800

    We can execute the sqlite statement now

--- GenericDB/mainwindow.cpp
+++ GenericDB/mainwindow.cpp
@@ -41,3 +41,96 @@ void MainWindow::CloseDatabase()
         mpDatabase = NULL;
     }
 }
+
+bool MainWindow::ExecuteSql(const QString& sql, DatabaseQueryExecutor* executor)
+{
+    if (mpDatabase == NULL || sql.isEmpty())
+        return false;
+
+    QByteArray sqlByteArray = sql.toUtf8();
+
+    int len = sqlByteArray.length();
+    char* sSql = new char[len + 1];
+    sSql[len] = 0;
+
+#ifdef AD_OS_WIN
+    strcpy_s(sSql, len+1, sqlByteArray.constData());
+#else
+    strcpy(sSql, sqlByteArray.constData());
+#endif
+
+    bool bRet = true;
+    sqlite3_stmt* pStmt = NULL;
+    const char* pTail = NULL;
+    int rc = sqlite3_prepare(mpDatabase, sSql, len, &pStmt, &pTail);
+    Q_ASSERT(rc == SQLITE_OK);
+    if (rc == SQLITE_OK)
+    {
+        rc = sqlite3_step(pStmt);
+        Q_ASSERT(rc == SQLITE_DONE || rc == SQLITE_ROW);
+        if (rc == SQLITE_DONE)
+            bRet = true;
+
+        while (rc == SQLITE_ROW)
+        {
+            if (executor == NULL)
+            {
+                bRet = false;
+                break;
+            }
+
+            executor->BuildResult(pStmt);
+            rc = sqlite3_step(pStmt);
+        }
+    }
+    else
+        bRet = false;
+
+    sqlite3_finalize(pStmt);
+    delete []sSql;
+
+    return bRet;
+}
+
+
+SingleColumnDatabaseQueryExecutor::SingleColumnDatabaseQueryExecutor()
+{
+    mpResult = new QString();
+}
+
+SingleColumnDatabaseQueryExecutor::~SingleColumnDatabaseQueryExecutor()
+{
+    if (mpResult != NULL)
+    {
+        delete (QString*) mpResult;
+        mpResult = NULL;
+    }
+}
+
+void SingleColumnDatabaseQueryExecutor::BuildResult(sqlite3_stmt* pStmt)
+{
+    if (pStmt == NULL)
+        return;
+
+    int count = sqlite3_column_count(pStmt);
+    Q_ASSERT(count == 1);
+    if (count == 1)
+        *((QString*)mpResult) = ((QString*)mpResult)->append(QString::fromUtf8((char*)sqlite3_column_blob(pStmt, 0)));
+
+}
+
+void SingleColumnDatabaseQueryExecutor::ClearResult()
+{
+    *((QString*)mpResult) = "";
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    QString sqlInsert = QString("insert into Configurations values(20,0,0,\"Folder\",\"Name\", 1)");
+    ExecuteSql(sqlInsert);
+
+
+    QString sqlDelete = QString("delete from Configurations where AIMKEY=20");
+    ExecuteSql(sqlDelete);
+}
--- GenericDB/mainwindow.h
+++ GenericDB/mainwindow.h
@@ -7,7 +7,10 @@ namespace Ui {
 class MainWindow;
 }
 
-class sqlite3;
+struct sqlite3;
+struct sqlite3_stmt;
+class DatabaseQueryExecutor;
+
 
 class MainWindow : public QMainWindow
 {
@@ -17,15 +20,44 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 
     sqlite3* mpDatabase;
     bool ConnectToDatabase(const QString& path);
     void CloseDatabase();
+    virtual bool ExecuteSql(const QString& sql, DatabaseQueryExecutor* executor = NULL);
 
 
 };
 
+
+class DatabaseQueryExecutor
+{
+public:
+    virtual ~DatabaseQueryExecutor(){};
+    virtual void BuildResult(sqlite3_stmt* pStmt) = 0;
+    void* GetResult();
+
+protected:
+    void *mpResult;
+};
+
+class SingleColumnDatabaseQueryExecutor : public DatabaseQueryExecutor
+{
+public:
+    SingleColumnDatabaseQueryExecutor();
+    ~SingleColumnDatabaseQueryExecutor();
+    virtual void BuildResult(sqlite3_stmt* pStmt);
+    void ClearResult();
+};
+
+inline void* DatabaseQueryExecutor::GetResult()
+{
+    return mpResult;
+}
 
 #endif // MAINWINDOW_H
