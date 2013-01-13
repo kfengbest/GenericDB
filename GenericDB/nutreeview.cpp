ff3e4f430b816a03fadb9db93c1232d2fb58f7ac
kfengbest <kfengbest@hotmail.com>
2013-01-13 21:15:07 +0800

    Mockup items for trees.

--- GenericDB/nutreeview.cpp
+++ GenericDB/nutreeview.cpp
@@ -8,9 +8,26 @@ NuTreeView::NuTreeView(NuViewBase* parentView, DmFolder* pData)
 {
     m_tree = new QTreeWidget;
     m_tree->setMaximumWidth(250);
+
 }
 
 void NuTreeView::onLoadView()
 {
+    QTreeWidgetItem* rootItem = new QTreeWidgetItem(QStringList() << "MyFolders");
+    m_tree->addTopLevelItem(rootItem);
 
+    QTreeWidgetItem* itemProj = new QTreeWidgetItem(QStringList() << "Projects");
+    QTreeWidgetItem* itemDocEng = new QTreeWidgetItem(QStringList() << "Engineering Document");
+    QTreeWidgetItem* rootDocOff = new QTreeWidgetItem(QStringList() << "Office Document");
+
+    QTreeWidgetItem* itemPerson = new QTreeWidgetItem(QStringList() << "Person");
+    QTreeWidgetItem* itemOrg = new QTreeWidgetItem(QStringList() << "Orgnization");
+
+    rootItem->addChild(itemProj);
+    rootItem->addChild(itemDocEng);
+    rootItem->addChild(rootDocOff);
+    rootItem->addChild(itemPerson);
+    rootItem->addChild(itemOrg);
+
+    m_tree->expandAll();
 }
